7f7e79552b3ea350e7b98eb4888446d17de13c12
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-03-24 21:15:37 +0900

    프로젝트 생성밑 기본 새팅 완료
    H2 데이터베이스를 사용하기에 따로 데이터베이스를 생성하였으며
    프로젝트의 자바 버전등을 수정하고
    오류를 해결하였다.

---
+++ src/main/java/park/mycommunitysite/MycommunitysiteApplication.java
@@ -0,0 +1,13 @@
+package park.mycommunitysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycommunitysiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycommunitysiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/park/mycommunitysite/MycommunitysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package park.mycommunitysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunitysiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
