89f71da83d1a5b4d091a6166356506f47b7ba487
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-04-07 20:18:10 +0900

    sql파일 : 프로젝트 개발동안 데이터를 미리 입력할 sql 문 작성하기
    applicaion.yml : 프로젝트 파일에 있는 sql 문을 찾아 실행시키게한다.
    중요
    jpa: defer-datasource-initialization : true 으로 설정해두어 아직 ddl이 실행되지 않아 테이블이 생성되기전 sql 문이 실행되는 것을 방지한다.
    
    그외에 추가한 부분은 내가 지정한 sql 파일을 프로젝트가 실행될 때마다 실행된다.
    
    memberData.sql : 회원 정보 관련하여 실행할 sql 문을 입력한다.
    주의점
    jpa 에서 @GerneratedValue 에의한  h2 데이터베이스의 id값 증가는 시퀸스 방식으로 설정해주기 때문에
    기존 mysql 처럼 insert 문을 작성하지 않고  next value for members_seq 을 사용한다. (오라클 방식으로 사용해도 오류가 난다.)
    아니면은 h2 db를 설정하여 방식을 수정하여도된다.
    
    결과적으로 회원 데이터가 2개가 생성되었다.

---
+++ src/main/resources/sql/memberData.sql
@@ -0,0 +1,8 @@
+/* 스프링 부트 JPA는 자체적으로 엔티티 클래스의 자동증가 규칙을 인색해서 처리해주지만
+   h2 데이터베이스는 데이터베이스가 생성되고 외래키까지는 알아서 해주지만 자동 증가 는 적용되지 않아
+   여기서 테이블을 설정해줘야한다.*/
+/* h2 데이터베이스는 시퀸스 방식을 사용하기 때문에 id 값을 시퀸스로 작성해줘야한다.
+    예 : next value for members_seq 단 해당 방식은 id 값이  50씩 증가해 여기서 2개의 데이터를 출력하면
+    1, 51 이 id 값으로 지정된다 테스트에는 문제가 없으나 실사용은 하면안된다. */
+insert into members (member_id,email,password,roles) values (next value for members_seq,'1@1','$2a$10$cyVvBSgLpuz43Lz4pSN7F.sjMhRk8b4VEMvaavLIBrciVgb0jLuli','USER,');
+insert into members (member_id,email,password,roles) values (next value for members_seq,'1@2','$2a$10$cyVvBSgLpuz43Lz4pSN7F.sjMhRk8b4VEMvaavLIBrciVgb0jLuli','USER,');
