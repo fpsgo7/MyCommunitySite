6b87690f8bfb36370db1c35c8129794d097472ad
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-04-09 23:55:15 +0900

    스프링 시큐리티 : 주석 추가와 토큰 설정값 수정
    CustomUserDetails : 해당 메서드의 실행 하는 경우에 대한 주석을 추가하였고 출력문으로 확인을 해보았다.
    JwtProperties : 토큰의 기간이 너무 짧아 수정하였다.

--- src/main/java/park/mycommunitysite/config/auth/CustomUserDetails.java
+++ src/main/java/park/mycommunitysite/config/auth/CustomUserDetails.java
@@ -22,12 +22,14 @@ public class CustomUserDetails implements UserDetails {
     /**
      * Member 객체의 권한을 저장하는 String 값에서
      * 권한들을 추출하여 리스트에 담는다.
+     * 권환 확인이 필요한 문장이 실행될 때마다 실행된다.
      * @return 유저 권한 리스트
      */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
         member.getRoleList().forEach(r->{
+            //System.out.println(r);
             authorities.add(()->r);
         });
         return authorities;
--- src/main/java/park/mycommunitysite/config/jwt/JwtProperties.java
+++ src/main/java/park/mycommunitysite/config/jwt/JwtProperties.java
@@ -6,7 +6,7 @@ package park.mycommunitysite.config.jwt;
 public interface JwtProperties {
     String tokenSubject  = "parkToken";
     String SECRET = "parkCommunity"; // 우리 서버만 알고있는 비밀값
-    int EXPIRATION_TIME = 60000*10; // 10분
+    int EXPIRATION_TIME = 60000*180; // 180분
     String TOKEN_PREFIX = "BEARER ";
     String HEADER_STRING = "Authorization";
 }
